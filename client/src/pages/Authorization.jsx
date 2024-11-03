5297bb70dc5ade49fea5f3bd4cbfbc089ff523d4
Arshad-Shamim <arshadshamim555@gmail.com>
2024-11-03 12:21:27 +0530

    write commint

--- client/src/pages/Authorization.jsx
+++ client/src/pages/Authorization.jsx
@@ -32,6 +32,7 @@ export default function Authorization() {
         "password":e.target.password.value
     }
     
+    //for guest;
     if(data.username=="guest"&&data.password=="guest"){
       localStorage.setItem("guest",1);
       setLink("/Memes/home")
